360a4fcc1cbc55836d9d90cc520fe40cffb22358
Serhii Plis <serzh2577@gmail.com>
2023-12-04 23:29:29 +0300

    task 1 gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,19 +1,29 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from './images.js';
 
-// <li class="gallery-item">
-//     <a class="gallery-link" href="large-image.jpg">
-//         <img
-//             class="gallery-image"
-//             src="small-image.jpg"
-//             alt="Image description"
-//             />
-//     </a>
-// </li>
+const galleryRef = document.querySelector('.gallery');
+
+const markupString = images
+  .map(
+    ({ preview, description, original }) => `
+<li class="gallery-item">
+<a class="gallery-link" href="${original}">
+    <img
+        class="gallery-image"
+        src="${preview}"
+        alt="${description}"
+        />
+</a>
+</li>
+`
+  )
+  .join('');
+
+galleryRef.innerHTML = markupString;
 
 const lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-
-// <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-// <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
